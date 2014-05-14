e5fb6341efc7b2553a0289dac1f178ec3cfd9d35
HungDaddy <lovestospoodge@hotmail.com>
2014-05-14 17:51:08 +0200

    ewuadhwuiada

--- Sd card/HeroWRt_assets.h
+++ Sd card/HeroWRt_assets.h
@@ -1,52 +1,12 @@
 #define LOAD_ASSETS()  GD.safeload("Media.GD2");
-#define 0_HANDLE 0
-#define 0_WIDTH 42
-#define 0_HEIGHT 45
-#define 0_CELLS 1
-#define 1_HANDLE 1
-#define 1_WIDTH 42
-#define 1_HEIGHT 45
-#define 1_CELLS 1
-#define 2_HANDLE 2
-#define 2_WIDTH 42
-#define 2_HEIGHT 45
-#define 2_CELLS 1
-#define 3_HANDLE 3
-#define 3_WIDTH 42
-#define 3_HEIGHT 45
-#define 3_CELLS 1
-#define 4_HANDLE 4
-#define 4_WIDTH 42
-#define 4_HEIGHT 45
-#define 4_CELLS 1
-#define 5_HANDLE 5
-#define 5_WIDTH 42
-#define 5_HEIGHT 45
-#define 5_CELLS 1
-#define 6_HANDLE 6
-#define 6_WIDTH 42
-#define 6_HEIGHT 45
-#define 6_CELLS 1
-#define 7_HANDLE 7
-#define 7_WIDTH 42
-#define 7_HEIGHT 45
-#define 7_CELLS 1
+#define SONICW_HANDLE 0
+#define SONICW_WIDTH 42
+#define SONICW_HEIGHT 45
+#define SONICW_CELLS 8
 #define SONICR0_HANDLE 8
 #define SONICR0_WIDTH 46
 #define SONICR0_HEIGHT 36
-#define SONICR0_CELLS 1
-#define SONICR1_HANDLE 9
-#define SONICR1_WIDTH 46
-#define SONICR1_HEIGHT 36
-#define SONICR1_CELLS 1
-#define SONICR2_HANDLE 10
-#define SONICR2_WIDTH 46
-#define SONICR2_HEIGHT 36
-#define SONICR2_CELLS 1
-#define SONICR3_HANDLE 11
-#define SONICR3_WIDTH 46
-#define SONICR3_HEIGHT 36
-#define SONICR3_CELLS 1
+#define SONICR0_CELLS 4
 #define TILE_HANDLE 12
 #define TILE_WIDTH 48
 #define TILE_HEIGHT 48
@@ -67,16 +27,6 @@
 #define JUMP_LENGTH 1280
 #define JUMP_FREQ 44100
 #define ASSETS_END 83128UL
-static const shape_t 0_SHAPE = {0, 42, 45, 0};
-static const shape_t 1_SHAPE = {1, 42, 45, 0};
-static const shape_t 2_SHAPE = {2, 42, 45, 0};
-static const shape_t 3_SHAPE = {3, 42, 45, 0};
-static const shape_t 4_SHAPE = {4, 42, 45, 0};
-static const shape_t 5_SHAPE = {5, 42, 45, 0};
-static const shape_t 6_SHAPE = {6, 42, 45, 0};
-static const shape_t 7_SHAPE = {7, 42, 45, 0};
-static const shape_t SONICR0_SHAPE = {8, 46, 36, 0};
-static const shape_t SONICR1_SHAPE = {9, 46, 36, 0};
-static const shape_t SONICR2_SHAPE = {10, 46, 36, 0};
-static const shape_t SONICR3_SHAPE = {11, 46, 36, 0};
+static const shape_t SONICW_SHAPE = {0, 42, 45, 0};
+static const shape_t SONICR_SHAPE = {8, 46, 36, 0};
 static const shape_t TILE_SHAPE = {12, 48, 48, 0};
