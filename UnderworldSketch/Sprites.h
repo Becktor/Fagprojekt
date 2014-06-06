d2d19aad6663dceddf492c62cd248c46a1d2e059
HungDaddy <lovestospoodge@hotmail.com>
2014-06-06 11:42:30 +0200

    Sprite Update + SD File

--- UnderworldSketch/Sprites.h
+++ UnderworldSketch/Sprites.h
@@ -7,31 +7,53 @@
 #define HEROA_WIDTH 55
 #define HEROA_HEIGHT 48
 #define HEROA_CELLS 2
-#define SONICR_HANDLE 2
-#define SONICR_WIDTH 46
-#define SONICR_HEIGHT 36
-#define SONICR_CELLS 4
-#define TILE_HANDLE 3
+#define HEROD_HANDLE 2
+#define HEROD_WIDTH 55
+#define HEROD_HEIGHT 48
+#define HEROD_CELLS 2
+#define MINOC_HANDLE 3
+#define MINOC_WIDTH 80
+#define MINOC_HEIGHT 72
+#define MINOC_CELLS 4
+#define MINOD_HANDLE 4
+#define MINOD_WIDTH 80
+#define MINOD_HEIGHT 72
+#define MINOD_CELLS 4
+#define MINOW_HANDLE 5
+#define MINOW_WIDTH 80
+#define MINOW_HEIGHT 72
+#define MINOW_CELLS 4
+#define TILE_HANDLE 6
 #define TILE_WIDTH 48
 #define TILE_HEIGHT 48
-#define TILE_CELLS 1
-#define ATTACK 70656UL
+#define TILE_CELLS 2
+#define DOOR_HANDLE 7
+#define DOOR_WIDTH 48
+#define DOOR_HEIGHT 48
+#define DOOR_CELLS 1
+#define ATTACK 215424UL
 #define ATTACK_LENGTH 1600
 #define ATTACK_FREQ 8000
-#define LAUGH 72256UL
+#define LAUGH 217024UL
 #define LAUGH_LENGTH 20000
 #define LAUGH_FREQ 8000
-#define JUMP 92256UL
+#define JUMP 237024UL
 #define JUMP_LENGTH 1280
 #define JUMP_FREQ 44100
-#define EXIT 93536UL
+#define EXIT 238304UL
 #define EXIT_LENGTH 4000
 #define EXIT_FREQ 8000
-#define COIN 97536UL
+#define COIN 242304UL
 #define COIN_LENGTH 8152
 #define COIN_FREQ 44100
-#define ASSETS_END 105688UL
+#define ASSETS_END 250456UL
 static const shape_t HEROW_SHAPE = {0, 55, 48, 0};
 static const shape_t HEROA_SHAPE = {1, 55, 48, 0};
-static const shape_t SONICR_SHAPE = {2, 46, 36, 0};
-static const shape_t TILE_SHAPE = {3, 48, 48, 0};
+static const shape_t HEROD_SHAPE = {2, 55, 48, 0};
+static const shape_t MINOC_SHAPE = {3, 80, 72, 0};
+static const shape_t MINOD_SHAPE = {4, 80, 72, 0};
+static const shape_t MINOW_SHAPE = {5, 80, 72, 0};
+static const shape_t TILE_SHAPE = {6, 48, 48, 0};
+static const shape_t DOOR_SHAPE = {7, 48, 48, 0};
+
+
