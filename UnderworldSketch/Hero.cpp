997e13783ae604a8fc06a72595794293ab5b1072
HungDaddy <lovestospoodge@hotmail.com>
2014-06-15 00:59:44 +0200

    Changed hero class (moved some shiz around)
    newHandle only gets called once per frame (except if the player attacks)

--- UnderworldSketch/Hero.cpp
+++ UnderworldSketch/Hero.cpp
@@ -6,9 +6,10 @@
 #include "Hero.h"
 #include "Sprites.h"
 
-Hero::Hero(ArduinoNunchuk* nunchuk) : Unit(HERO_HITBOX_WIDTH, HERO_HITBOX_HEIGHT_STAND, HERO_HEALTH, 0, HERO_IDLE_WIDTH),
-                                      _attackArea(HERO_ATT_RANGE, HERO_HITBOX_HEIGHT_STAND),
-                                      _attack(&_attackArea, HERO_ATT_DAMAGE, HERO_ATT_FORCE, this) {
+Hero::Hero(ArduinoNunchuk* nunchuk) : 
+Unit(HERO_HITBOX_WIDTH, HERO_HITBOX_HEIGHT_STAND, HERO_HEALTH, 0, HERO_IDLE_WIDTH),
+_attackArea(HERO_ATT_RANGE, HERO_HITBOX_HEIGHT_STAND),
+_attack(&_attackArea, HERO_ATT_DAMAGE, HERO_ATT_FORCE, this) {
   _nunchuk = nunchuk;
 }
 
@@ -25,67 +26,77 @@ void Hero::initialize() {
 void Hero::updateAI(byte dTime, Logic *logic) { //dtime is still unused
   //Hero x-movement
   char nunchukX = _nunchuk->analogX - NUNCHUK_REST_X,
-       nunchukDir = getDirection(nunchukX),
-       nunchukXAbs = nunchukX * nunchukDir,
-       acc,
-       targetSpeed;
-  if(nunchukXAbs >= NUNCHUK_WALK) {
-    byte FR;
-    if(nunchukXAbs >= NUNCHUK_RUN) {
-      acc = HERO_ACC_RUN;
-      targetSpeed = HERO_SPEED_RUN;
-      FR = HERO_FR_RUNNING;
-    } else {
-      acc = HERO_ACC_WALK;
-      targetSpeed = HERO_SPEED_WALK;
-      FR = HERO_FR_WALKING;
-    }
-    newHandle(HERO_MOVE_HANDLE, HERO_MOVE_CELLS, FR);
-    _dir = nunchukDir;
-    targetSpeed = nunchukDir * targetSpeed;
-  } else { //IDLE
-    acc = HERO_ACC_WALK;
-    targetSpeed = 0;
-    nunchukDir = 0;
-    newHandle(HERO_IDLE_HANDLE, HERO_IDLE_CELLS, HERO_FR_IDLE);
-  }
+  nunchukDir = getDirection(nunchukX),
+  nunchukXAbs = nunchukX * nunchukDir,
+  acc,
+  targetSpeed;
 
-  //Hero jump
   if(logic->isGrounded(this)) {
+
+    //Hero duck
+    if(NUNCHUK_DUCK > _nunchuk->analogY && !_isJumping) {
+      acc = HERO_ACC_DUCK;
+      targetSpeed = nunchukDir * HERO_SPEED_DUCK;
+      newHandle(HERO_DUCK_HANDLE, HERO_DUCK_CELLS, HERO_FR_DUCKING);
+      if(!_isDucking) {
+        _hitbox._height = HERO_HITBOX_HEIGHT_DUCK;
+        _hitbox._y += HERO_HITBOX_HEIGHT_STAND - HERO_HITBOX_HEIGHT_DUCK;
+        _isDucking = true;
+      }
+    } 
+    else if(_isDucking) {
+      _hitbox._height = HERO_HITBOX_HEIGHT_STAND;
+      _hitbox._y += HERO_HITBOX_HEIGHT_STAND - HERO_HITBOX_HEIGHT_DUCK;
+      _isDucking = false;
+
+    } 
+    else if(nunchukXAbs >= NUNCHUK_WALK){
+      byte FR;
+      if(nunchukXAbs >= NUNCHUK_RUN) {
+        acc = HERO_ACC_RUN;
+        targetSpeed = HERO_SPEED_RUN;
+        FR = HERO_FR_RUNNING;
+      } 
+      else {
+        acc = HERO_ACC_WALK;
+        targetSpeed = HERO_SPEED_WALK;
+        FR = HERO_FR_WALKING;
+      }
+      newHandle(HERO_MOVE_HANDLE, HERO_MOVE_CELLS, FR);
+      _dir = nunchukDir;
+      targetSpeed = nunchukDir * targetSpeed;
+    }
+    else {
+      acc = HERO_ACC_WALK;
+      targetSpeed = 0;
+      nunchukDir = 0;
+      newHandle(HERO_IDLE_HANDLE, HERO_IDLE_CELLS, HERO_FR_IDLE);
+    }
+
     if(_nunchuk->cButton) {
       if(!_isJumping) {
-        _yVel = -HERO_JUMP;
+        _yVel = HERO_JUMP;
         _isJumping = true;
         GD.sample(JUMP, JUMP_LENGTH, 8000, ADPCM_SAMPLES);
       }
-    } else
+    } 
+    else
       _isJumping = false;
-  } else {
+
+  } 
+  else {
     if(_yVel <= 0)
       newHandle(HERO_JUMP_HANDLE, HERO_JUMP_CELLS, HERO_FR_JUMP);
     else
       newHandle(HERO_FALL_HANDLE, HERO_FALL_CELLS, HERO_FR_FALL);
-    targetSpeed = nunchukDir * HERO_SPEED_AIR;
+    if(nunchukXAbs >= NUNCHUK_RUN)
+      targetSpeed = nunchukDir * HERO_SPEED_AIR;
+    else
+      targetSpeed =  0;
     if(HERO_SPEED_AIR > nunchukDir * _xVel)
       acc = HERO_ACC_AIR;
     else
       acc = 0;
-  }
-
-  //Hero duck
-  if(NUNCHUK_DUCK > _nunchuk->analogY && !_isJumping) {
-    acc = HERO_ACC_DUCK;
-    targetSpeed = nunchukDir * HERO_SPEED_DUCK;
-    newHandle(HERO_DUCK_HANDLE, HERO_DUCK_CELLS, HERO_FR_DUCKING);
-    if(!_isDucking) {
-      _isDucking = true;
-      _hitbox._height = HERO_HITBOX_HEIGHT_DUCK;
-      _hitbox._y += HERO_HITBOX_HEIGHT_STAND - HERO_HITBOX_HEIGHT_DUCK;
-    }
-  } else if(_isDucking) {
-    _hitbox._height = HERO_HITBOX_HEIGHT_STAND;
-    _hitbox._y += HERO_HITBOX_HEIGHT_DUCK - HERO_HITBOX_HEIGHT_STAND;
-    _isDucking = false;
   }
 
   _xVel = zoomIn(acc, _xVel, targetSpeed);
@@ -93,10 +104,12 @@ void Hero::updateAI(byte dTime, Logic *logic) { //dtime is still unused
   //Hero action
   _attackSound = false;
   if(_nunchuk->zButton) {
-    if(_isDucking && logic->atExit(this)) { //Exit
+    if(_isDucking && logic->atExit(this)) {
       logic->_gameOver = true;
       logic->_heroWin = true;
-    } else if(!_isAttacking) { //Attack
+    } 
+    else if(!_isAttacking && !_isDucking) {
+      //Attack
       _isAttacking = true;
       if(_dir == LEFT)
         _attackArea._x = _hitbox._x - HERO_ATT_RANGE - 1;
@@ -109,10 +122,12 @@ void Hero::updateAI(byte dTime, Logic *logic) { //dtime is still unused
       _animLock = true;
       _attackSound = true;
     }
-  } else
+  } 
+  else
     _isAttacking = false;
 }
 
 boolean Hero::getAttackSound() {
   return _attackSound;
 }
+
